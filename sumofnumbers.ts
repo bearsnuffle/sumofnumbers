59b5ee12d6dac49f01cff671bd12f1aa3f81a42c
bearsnuffle <hungryeaglehms@gmail.com>
2025-06-11 22:13:48 -1000

    finished

---
+++ sumofnumbers.ts
@@ -0,0 +1,33 @@
+function sumFor(list: number[]) {
+  let total = 0;
+  for (let i = 0; i < list.length; i++) {
+    total += list[i];
+  }
+  return total;
+}
+
+function sumWhile(list: number[]) {
+  let total = 0;
+  let i = 0;
+  while (i < list.length) {
+    total += list[i];
+    i++;
+  }
+  return total;
+}
+
+function sumRecursion(list: number[]): number {
+  if (list.length === 0) {
+    return 0;
+  }
+  return list[0] + sumRecursion(list.slice(1));
+}
+
+function sumTheFunctionalWay(list: number[]) {
+  return list.reduce((total, currentValue) => total + currentValue, 0);
+}
+
+console.log(sumFor([1, 2, 3, 4]));
+console.log(sumWhile([1, 2, 3, 4]));
+console.log(sumRecursion([1, 2, 3, 4]));
+console.log(sumTheFunctionalWay([1, 2, 3, 4]));
